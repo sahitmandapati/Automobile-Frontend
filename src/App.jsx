21eb16afb55e2d20ea2ce8314a27afee8092a662
sahitmandapati <sahit.m@technovert.com>
2023-03-18 13:41:12 +0530

    create a mock server using mirage.js and have some mock data

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
 import './App.css'
 import Home from './components/Home'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import About from './components/About'
+
+import "../server"
 function App() {
 
   return (
