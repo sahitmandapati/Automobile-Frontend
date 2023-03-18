e959f43ae4a80ee7959a45f464a98600670f4e15
sahitmandapati <sahit.m@technovert.com>
2023-03-18 16:59:06 +0530

    added padding bottom to about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const About = () => {
     return (
-        <div className='about bg-[#FFF7ED]'>
+        <div className='about bg-[#FFF7ED] pb-5'>
             <div className="aboutImage h-60"></div>
             <section className="heading w-10/12 mx-auto flex items-center font-extrabold text-4xl my-5">
                 Don't squeeze in a sedan when you could relax in a van.
@@ -14,7 +14,7 @@ const About = () => {
 
                 <p className='my-2'>Our team is full of vanlife enthusiasts who know firsthand the magic of touring the world on 4 wheels.</p>
             </section>
-            <section className="highlight px-10 py-14 bg-[#FFCC8D] w-10/12 m-auto rounded my-5">
+            <section className="highlight px-10 py-14 bg-[#FFCC8D] w-10/12 m-auto rounded">
                 <h1 className='font-bold text-2xl my-2'>Your destination is waiting.
                     Your van is ready.</h1>
                 <button className='bg-[#161616] hover:bg-[#232323] text-white font-bold py-2 px-4 rounded my-2'><Link to='/vans'>Explore our vans</Link></button>
