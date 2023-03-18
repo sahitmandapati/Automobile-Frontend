7bb2f50109c3f5b7557031c140672aeefbd54b15
sahitmandapati <sahit.m@technovert.com>
2023-03-18 13:14:09 +0530

    refactor style for responsiveness

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
         <div id="header" className='flex justify-between items-center px-10 h-28 bg-[#FFF7ED]'>
             <div className="logo text-3xl font-extrabold"><Link to='/'>#vanlife</Link></div>
             <nav className="navigation">
-                <ul className='flex text-[#4D4D4D] font-semibold w-40 justify-between'>
+                <ul className='flex text-[#4D4D4D] font-semibold w-24 justify-between'>
                     <li><Link to='/about'>About</Link> </li>
                     <li><a href="#">Vans</a></li>
                 </ul>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div id="main" className='h-96 text-[#ffff] content flex flex-col text-left justify-evenly'>
-            <section className='w-2/4 m-auto'>
+        <div id="main" className='text-[#ffff] content flex flex-col text-left justify-evenly py-10 lg:h-96'>
+            <section className='w-3/4 m-auto py-10'>
                 <h1 className='font-extrabold text-4xl mb-10'>You got the travel plans, we got the travel vans.</h1>
                 <p className=''>Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
             </section>
-            <button className='w-2/4 m-auto bg-[#ff974e] hover:bg-[#FF8C38] text-white font-bold py-2 px-4 rounded'>Find your van</button>
+            <button className='w-3/4 m-auto bg-[#ff974e] hover:bg-[#FF8C38] text-white font-bold py-2 px-4 rounded'>Find your van</button>
         </div>
     );
 }
